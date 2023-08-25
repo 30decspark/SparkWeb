09fefb3662710a064ce0b404bbeaca972ee685b8
30decspark <30decspark@gmail.com>
2023-08-26 01:26:53 +0700

    finish SparkWeb.DotEnv namespace, for load .env to .net project

---
+++ DotEnv/DotEnv.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SparkWeb.DotEnv
+{
+    public class DotEnv
+    {
+        public static void Load()
+        {
+            string file = Directory.GetParent(AppContext.BaseDirectory).FullName + "\\.env";
+            if (!File.Exists(file)) return;
+            string[] lines = File.ReadAllLines(file);
+            var env = new ExpandoObject() as IDictionary<string, object>;
+            foreach (string s in lines)
+            {
+                if (s.Trim() == string.Empty) continue;
+                if (s.Trim().StartsWith("#")) continue;
+                string[] sp = s.Split('=');
+                if (sp.Length < 2) return;
+                string key = sp[0];
+                if (!Regex.IsMatch(key, "^[A-Z]+(_[A-Z]+)*$")) continue;
+                string value = sp[1];
+                env[key] = value;
+            }
+            process.env = env;
+        }
+    }
+}
---
+++ DotEnv/process.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkWeb.DotEnv
+{
+    public class process
+    {
+        public static dynamic env { get; internal set; }
+    }
+}
