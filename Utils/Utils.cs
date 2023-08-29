98525b092c30824c127249b1d0eb2144eded91a8
30decspark <30decspark@gmail.com>
2023-08-29 16:30:17 +0700

    add Utils to project

---
+++ Utils/Utils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkWeb.Utils
+{
+    public class Utils
+    {
+        public static string GetPath()
+        {
+            return Directory.GetParent(AppContext.BaseDirectory).FullName;
+        }
+
+        public static string GetShellName()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return "cmd.exe";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                return "zsh";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return "bash";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
+            {
+                return "tcsh";
+            }
+            else
+            {
+                throw new PlatformNotSupportedException("Unsupported operating system.");
+            }
+        }
+    }
+}
